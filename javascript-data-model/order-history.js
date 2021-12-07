af893a368392ec0ab877579b4ada79c5703d10db
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-07 12:32:44 -0800

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,125 @@
+var orderHistory = [
+  {
+    items: [
+      {
+        item: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel'
+      }
+    ],
+    itemImage: '',
+    orderId: '114-3941689-8772232',
+    orderDetails: '<a>Order Details</a>',
+    invoice: '<a>Invoice</a>',
+    orderDate: 'August 4, 2020',
+    deliverDate: 'Aug 8, 2020',
+    deliveredDirectly: false,
+    subTotal: 31.55,
+    total: 34,
+    shipTo: 'JS Masher',
+    reviewed: false,
+    returnWindow: false,
+    buyAgain: false,
+    viewItem: false,
+    productSupport: false
+  },
+
+  {
+    items: [
+      {
+        item: 'The Timeless Way of Building',
+        author: 'Alaxander, Christopher'
+      }
+    ],
+    itemImage: '',
+    orderId: '113-9984268-1280257',
+    orderDetails: '<a>Order Details</a>',
+    invoice: '<a>Invoice</a>',
+    orderDate: 'July 19, 2020',
+    deliverDate: 'Jul 20, 2020',
+    deliveredDirectly: true,
+    subTotal: 41.33,
+    total: 44.53,
+    shipTo: 'JS Masher',
+    reviewed: false,
+    returnWindow: false,
+    buyAgain: false,
+    viewItem: false,
+    productSupport: false
+  },
+
+  {
+    items: 'Gamecube Controller Adapter',
+    itemImage: '',
+    orderId: '114-2875557-9059409',
+    orderDetails: '<a>Order Details</a>',
+    invoice: '<a>Invoice</a>',
+    orderDate: 'July 4, 2020',
+    deliverDate: 'Jul 7, 2020',
+    deliveredDirectly: true,
+    subTotal: 15.98,
+    total: 17.22,
+    shipTo: 'JS Masher',
+    reviewed: false,
+    returnWindow: false,
+    buyAgain: false,
+    viewItem: false,
+    productSupport: false
+  },
+
+  {
+    items: [
+      {
+        item1: 'GameCube Controller'
+      },
+      {
+        item2: 'The Art of Sql',
+        author: 'Faroult, Stephane'
+      }
+    ],
+    itemImage: ['img1', 'img2'],
+    orderId: '113-2883177-2648248',
+    orderDetails: '<a>Order Details</a>',
+    invoice: '<a>Invoice</a>',
+    orderDate: 'July 3, 2020',
+    deliverDate: 'Jul 5, 2020',
+    deliveredDirectly: false,
+    subTotal: [
+      {
+        item1: 94.95,
+        item2: 33.99
+      }
+    ],
+    total: 138.93,
+    shipTo: 'JS Masher',
+    reviewed: [
+      {
+        item1: false,
+        item2: true
+      }
+    ],
+    returnWindow: [
+      {
+        item1: false,
+        item2: false
+      }
+    ],
+    buyAgain: [
+      {
+        item1: false,
+        item2: false
+      }
+    ],
+    viewItem: [
+      {
+        item1: false,
+        item2: false
+      }
+    ],
+    productSupport: [
+      {
+        item1: true,
+        item2: false
+      }
+    ]
+  }
+];
