ba549fbc1506543ac00ad46fdea6d7ccbede8cd3
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-09 20:53:49 -0800

    Completed javascript-loops-practice

---
+++ javascript-loops-practice/filter-out-nulls.js
@@ -0,0 +1,10 @@
+/* exported filterOutNulls */
+function filterOutNulls(values) {
+  var newArray = [];
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      newArray.push(values[i]);
+    }
+  }
+  return newArray;
+}
