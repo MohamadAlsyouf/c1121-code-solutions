6b057f1411dfa6612f2a0d3526ca06d315bb177d
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-14 14:10:08 -0800

    Completed string-manipulation-1

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,18 +1,12 @@
 /* exported reverseWord */
-// create storage for reversed word
-// create a copy of the word with its order of characters reversed
-// store that copy of the word
+// create storage for flipped word
+// go through each letter of the word starting from the last letter
+// add the last letter to the storage of flipped letters
 // give back the reversed word
-
-// function reverseWord(word) {
-//   // var wordReverse = '';
-//   var wordReverse = word.lastIndexOf(word.length);
-//   return wordReverse;
-// }
 function reverseWord(word) {
   var flippedWord = '';
   for (var i = word.length - 1; i >= 0; i--) {
-    flippedWord = flippedWord + word[i];
+    flippedWord += word[i];
   }
   return flippedWord;
 }
