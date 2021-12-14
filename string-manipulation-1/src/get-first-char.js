a7c7efe3decea16a9821cca0d8afc4a61501c9f5
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-14 15:07:48 -0800

    Completed string-manipulation-1

--- string-manipulation-1/src/get-first-char.js
+++ string-manipulation-1/src/get-first-char.js
@@ -1,9 +1,7 @@
 /* exported getFirstChar */
-// storage for the first character in a string.
 // get the first character value of the string and store it.
 // give back the location of stored first character of the string.
 function getFirstChar(string) {
-  var firstChar = [];
-  firstChar = string[0];
+  var firstChar = string[0];
   return firstChar;
 }
--- string-manipulation-1/src/get-last-char.js
+++ string-manipulation-1/src/get-last-char.js
@@ -1,9 +1,7 @@
 /* exported getLastChar */
-// storage for the last character in a string.
 // find and store the last character value of the string
 // give back the location of stored last character of said string
 function getLastChar(string) {
-  var lastChar = [];
-  lastChar = string[string.length - 1];
+  var lastChar = string[string.length - 1];
   return lastChar;
 }
