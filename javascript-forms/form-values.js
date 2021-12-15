7f29575683f0e7fafa3cd9374937574c777abbe9
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-15 14:28:26 -0800

    Completed javascript-forms exercise.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,12 @@
+var $formElements = document.querySelector('#contact-form');
+
+function handleSubmit(event) {
+  var name = $formElements.elements.name.value;
+  var email = $formElements.elements.email.value;
+  var message = $formElements.elements.message.value;
+  console.log({ name, email, message });
+  event.preventDefault();
+}
+
+var $contactForm = document.querySelector('#contact-form');
+$contactForm.addEventListener('submit', handleSubmit);
