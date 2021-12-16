2f6a4d7e6ccbe18dc1197ae60a2d669a9a30156e
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-15 21:23:32 -0800

    Completed object-manipulation-1

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,7 +1,9 @@
 /* exported toObject */
-//
-//
-//
+// store the first item in the keyValuePair list given to us
+// store the second item in that list in another storage location
+// make an empty list storage of key value pairs
+// set the 2nd item given to us in the list equal to the 1st item
+// give back location of the empty list we made, now full.
 function toObject(keyValuePair) {
   var key = keyValuePair[0];
   var value = keyValuePair[1];
