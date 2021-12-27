5e3d4445f1d5b677a26a28fa54bdd5ad241da9c3
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-12-27 12:47:14 -0800

    Completed javascript-this exercise

---
+++ javascript-this/student.js
@@ -0,0 +1,14 @@
+/* exported student */
+var student = {
+  firstName: 'Mohamad',
+  lastName: 'Alsyouf',
+  subject: 'JavaScript',
+  getFullName: function () {
+    var fullName = this.firstName + ' ' + this.lastName;
+    return fullName;
+  },
+  getIntroduction: function () {
+    var introduction = 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+    return introduction;
+  }
+};
