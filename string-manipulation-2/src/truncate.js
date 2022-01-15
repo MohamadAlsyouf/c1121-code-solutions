3f843873ef9d2708bb8dc7f4a1f4c607e9987f3d
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-01-15 11:21:14 -0800

    Revised Completed String Manipulation 2

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -3,8 +3,9 @@
 // take the string values up until a certain point (length)
 // move that new string into storage
 // add ellipsis to the end of the new string
-var newString = '';
+
 function truncate(length, string) {
+  var newString = '';
   newString = string.slice(0, length);
   newString += '...';
   return newString;
