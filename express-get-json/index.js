b46654be81de267f48a9e7cbada46bc9a3f17c50
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-01-24 20:38:33 -0800

    Completed express get-json exercise

---
+++ express-get-json/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradeArray = [];
+  for (var grade in grades) {
+    gradeArray.push(grades[grade]);
+  }
+  res.json(gradeArray);
+});
