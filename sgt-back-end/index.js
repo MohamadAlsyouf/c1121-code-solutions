8522b374dcbbeff6549d211c287d577d74ff422b
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-01-30 23:05:08 -0800

    Finished feature 3, PUT, started feature 4, DEL

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const pg = require('pg');
-// only create ONE pool for your whole server
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
   ssl: {
@@ -9,7 +8,7 @@ const db = new pg.Pool({
   }
 });
 
-// get - return all rows from grade table
+// returns all rows from grade table
 app.get('/api/grades', (req, res) => {
   const sql = `
     select *
@@ -29,10 +28,24 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.use(express.json());
-
+// inserts a new grade into the grades table and returns the new grade
 app.post('/api/grades', (req, res) => {
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = parseInt(req.body.score);
   const text = 'INSERT INTO grades(name, course, score) VALUES($1, $2, $3) RETURNING *';
-  const values = [req.body.name, req.body.course, req.body.score];
+  const values = [name, course, score];
+
+  if (name === undefined || course === undefined || score === undefined) {
+    res.status(400).json({ error: 'missing name, course, or score!' });
+    return;
+  } else if (score < 0 || score > 100) {
+    res.status(400).json({ error: 'score is not an integer from 0-100!' });
+    return;
+  } else if (!Number.isInteger(score)) {
+    res.status(400).json({ error: 'score is not an integer!' });
+    return;
+  }
 
   db.query(text, values)
     .then(result => {
@@ -41,15 +54,63 @@ app.post('/api/grades', (req, res) => {
 
     })
     .catch(err => {
-      if (values.name === undefined) {
-        res.status(400).json({ error: 'missing name!' });
-
-      } else {
-        console.error(err);
-        res.status(500).json({ error: 'An unexpected error occured' });
-      }
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
 
     });
+});
+
+// updated a grade in the grades table and returns the updated grade
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = parseInt(req.body.score);
+
+  const sql = `
+    UPDATE "grades"
+    SET "poop" = $1,
+        "butt" = $2,
+        "face" = $3
+    WHERE "gradeId" = ${gradeId}
+    RETURNING *`;
+  const values = [name, course, score];
+
+  if (name === undefined || course === undefined || score === undefined) {
+    res.status(400).json({ error: 'missing name, course, or score!' });
+    return;
+  } else if (score < 0 || score > 100) {
+    res.status(400).json({ error: 'score is not an integer from 0-100!' });
+    return;
+  } else if (!Number.isInteger(score)) {
+    res.status(400).json({ error: 'score is not an integer!' });
+    return;
+  } else if (gradeId === undefined) {
+    res.status(404).json({ error: 'gradeId does not exist in grades table!' });
+    return;
+  }
+
+  db.query(sql, values)
+    .then(result => {
+      const updatedGrade = result.rows[0];
+      res.status(200).json(updatedGrade);
+
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+      // my 500 error is not working right !!!
+    });
+});
+
+// deletes a grade from the grades table
+app.delete('/api/grades/:gradeId', (req, res) => {
+  // const deleteGrade = Number(req.params.gradeId);
 });
 
 // connects to host
