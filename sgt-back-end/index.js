752c28eadb007742937535b4052df1eb221e6277
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-01-31 09:54:38 -0800

    Completed sgt back end Challenge

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -8,7 +8,6 @@ const db = new pg.Pool({
   }
 });
 
-// returns all rows from grade table
 app.get('/api/grades', (req, res) => {
   const sql = `
     select *
@@ -28,15 +27,19 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.use(express.json());
-// inserts a new grade into the grades table and returns the new grade
+
 app.post('/api/grades', (req, res) => {
   const name = req.body.name;
   const course = req.body.course;
   const score = parseInt(req.body.score);
-  const text = 'INSERT INTO grades(name, course, score) VALUES($1, $2, $3) RETURNING *';
+  const sql = `
+  INSERT INTO grades(name, course, score)
+  VALUES($1, $2, $3)
+  RETURNING *
+  `;
   const values = [name, course, score];
 
-  if (name === undefined || course === undefined || score === undefined) {
+  if (!name || !course || !score) {
     res.status(400).json({ error: 'missing name, course, or score!' });
     return;
   } else if (score < 0 || score > 100) {
@@ -47,7 +50,7 @@ app.post('/api/grades', (req, res) => {
     return;
   }
 
-  db.query(text, values)
+  db.query(sql, values)
     .then(result => {
       const newGrade = result.rows[0];
       res.status(201).json(newGrade);
@@ -62,58 +65,71 @@ app.post('/api/grades', (req, res) => {
     });
 });
 
-// updated a grade in the grades table and returns the updated grade
 app.put('/api/grades/:gradeId', (req, res) => {
   const gradeId = Number(req.params.gradeId);
-
   const name = req.body.name;
   const course = req.body.course;
   const score = parseInt(req.body.score);
-
   const sql = `
     UPDATE "grades"
-    SET "poop" = $1,
-        "butt" = $2,
-        "face" = $3
+    SET "name" = $1,
+        "course" = $2,
+        "score" = $3
     WHERE "gradeId" = ${gradeId}
-    RETURNING *`;
+    RETURNING *
+    `;
   const values = [name, course, score];
 
-  if (name === undefined || course === undefined || score === undefined) {
+  if (!name || !course || !score) {
     res.status(400).json({ error: 'missing name, course, or score!' });
     return;
   } else if (score < 0 || score > 100) {
     res.status(400).json({ error: 'score is not an integer from 0-100!' });
-    return;
-  } else if (!Number.isInteger(score)) {
-    res.status(400).json({ error: 'score is not an integer!' });
-    return;
-  } else if (gradeId === undefined) {
-    res.status(404).json({ error: 'gradeId does not exist in grades table!' });
     return;
   }
 
   db.query(sql, values)
     .then(result => {
       const updatedGrade = result.rows[0];
-      res.status(200).json(updatedGrade);
-
+      if (!updatedGrade) {
+        res.status(404).json({ error: 'gradeId does not exist in grades table!' });
+      } else {
+        res.status(200).json(updatedGrade);
+      }
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({
-        error: 'An unexpected error occurred.'
-      });
-      // my 500 error is not working right !!!
+      res.status(500).json({ error: 'An unexpected error occurred.' });
     });
 });
 
-// deletes a grade from the grades table
 app.delete('/api/grades/:gradeId', (req, res) => {
-  // const deleteGrade = Number(req.params.gradeId);
+  const gradeId = Number(req.params.gradeId);
+  const sql = `
+  DELETE FROM "grades"
+  WHERE "gradeId" = ${gradeId}
+  RETURNING *
+  `;
+  if (!Number(gradeId)) {
+    res.status(400).json({ error: 'Invalid gradeId!' });
+    return;
+  }
+
+  db.query(sql)
+    .then(result => {
+      const deletedGrade = result.rows[0];
+      if (!deletedGrade) {
+        res.status(404).json({ error: 'gradeId does not exist in grades table!' });
+      } else {
+        res.status(204).json(deletedGrade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occured.' });
+    });
 });
 
-// connects to host
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Express server listening on PORT 3000');
