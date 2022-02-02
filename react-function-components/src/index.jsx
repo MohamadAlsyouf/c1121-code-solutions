d1531215e5465d6864aed37c7995a7a3bbdecf0f
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-02-02 11:25:54 -0800

    Completed react function components exercise

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton() {
+  return <button>Click Me!</button>;
+}
+
+ReactDOM.render(
+  <CustomButton/>,
+  document.getElementById('root')
+);
