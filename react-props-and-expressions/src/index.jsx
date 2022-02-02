6d435d2df9b5825b6a3f2f1af9f39f50e47917e0
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-02-02 12:40:22 -0800

    Completed react props and expressions exercise

---
+++ react-props-and-expressions/src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton(props) {
+  return (
+    <button>{props.text}</button>
+  );
+}
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="think" />
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
