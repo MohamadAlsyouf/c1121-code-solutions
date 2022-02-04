a4cd1dfb695597cb19f27d349d97155a30d82f0d
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-02-04 10:39:14 -0800

    Completed react form controls exercise

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsLetterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ value: '' });
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+          <input type="text" onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Sign Up" />
+      </form>
+    );
+  }
+}
+export default NewsLetterForm;
+
+ReactDOM.render(
+  <NewsLetterForm />,
+  document.getElementById('root')
+);
