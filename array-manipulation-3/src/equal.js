5168e3cd9264598ece45c22877c635bcfe2da1f6
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-03-24 11:05:56 -0700

    Refactored equal function conditionals

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,12 +1,17 @@
 /* exported equal */
+// if the length of the first array is not equal to the length of second array
+//  return false
 // iterate over the indexes of the first array
-// if index of first array is not equal to index of second array or if they aren't equal length,
+// if index of first array is not equal to index of second array,
 //  return false
 // otherwise, return true
 
 const equal = (first, second) => {
+  if (first.length !== second.length) {
+    return false;
+  }
   for (var i = 0; i < first.length; i++) {
-    if (first[i] !== second[i] || first.length !== second.length) {
+    if (first[i] !== second[i]) {
       return false;
     }
   }
